1d9381616a8ac13d58c4a540a61af05d41fa2dfd
DevNadimm <nadimchowdhury87@gmail.com>
2024-09-21 16:24:10 +0600

    initial

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,9 +1,13 @@
 import 'package:firebase_crud/ui/create_contact.dart';
 import 'package:firebase_crud/widgets/logout_dialog.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+
+  final DatabaseReference ref = FirebaseDatabase.instance.ref('Contact');
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +37,7 @@ class HomeScreen extends StatelessWidget {
           Navigator.push(
             context,
             MaterialPageRoute(
-              builder: (_) => CreateContact(),
+              builder: (_) => const CreateContact(),
             ),
           );
         },
@@ -41,8 +45,41 @@ class HomeScreen extends StatelessWidget {
         foregroundColor: Colors.white,
         child: const Icon(Icons.add_box),
       ),
-      body: const Center(
-        child: Text('Home Screen Content'),
+      body: Column(
+        children: [
+          Expanded(
+            child: FirebaseAnimatedList(
+              query: ref,
+              defaultChild: const Center(
+                child: CircularProgressIndicator(
+                  color: Colors.deepPurple,
+                ),
+              ),
+              itemBuilder: (context, snapshot, animation, index) {
+                String name = snapshot.child('name').value.toString();
+                String contact = snapshot.child('contact').value.toString();
+
+                return ListTile(
+                  title: Text(
+                    name,
+                    style: const TextStyle(fontWeight: FontWeight.w600),
+                  ),
+                  subtitle: Text(
+                    contact,
+                    style: const TextStyle(fontWeight: FontWeight.w500),
+                  ),
+                  leading: CircleAvatar(
+                    backgroundColor: Colors.deepPurple,
+                    child: Text(
+                      name[0].toUpperCase(),
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
